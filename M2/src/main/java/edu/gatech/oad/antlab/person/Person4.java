cbef4fe6a496f80383b9e156c609a3b52431feff
Janae Crawford <46799979+janaecrawford@users.noreply.github.com>
2019-01-25 15:38:10 -0500

    Update Person4.java

--- M2/src/main/java/edu/gatech/oad/antlab/person/Person4.java
+++ M2/src/main/java/edu/gatech/oad/antlab/person/Person4.java
@@ -31,7 +31,13 @@ public class Person4 {
      */
     private String calc(String input) {
       //Person 4 put your implementation here
-      return null;
+      String output = input;
+      for (int i = 0; i < input.length(); i++) {
+          char newChar = input.charAt(i);
+          newChar += 1;
+          output.replace(input.charAt(i), newChar);
+      }
+      return output;
     }
     
     /**
